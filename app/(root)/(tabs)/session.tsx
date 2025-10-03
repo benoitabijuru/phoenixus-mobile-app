2140dc2e070056cf69e1101cfaa490997f7b2bba
benoitabijuru <abinoit2@gmail.com>
2025-10-03 06:38:34 +0200

    route and authentication

---
+++ app/(root)/(tabs)/session.tsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from 'react-native'
+
+function Session() {
+  return (
+   <View><Text>Session Page</Text></View>
+  )
+}
+
+export default Session
---
+++ constants/index.ts
@@ -0,0 +1,66 @@
+export const icons = {
+  arrowDown: require("@/assets/icons/arrow-down.png"),
+  arrowUp: require("@/assets/icons/arrow-up.png"),
+  backArrow: require("@/assets/icons/back-arrow.png"),
+  chat: require("@/assets/icons/chat.png"),
+  checkmark: require("@/assets/icons/check.png"),
+  close: require("@/assets/icons/close.png"),
+  dollar: require("@/assets/icons/dollar.png"),
+  email: require("@/assets/icons/email.png"),
+  eyecross: require("@/assets/icons/eyecross.png"),
+  google: require("@/assets/icons/google.png"),
+  home: require("@/assets/icons/home.png"),
+  list: require("@/assets/icons/list.png"),
+  lock: require("@/assets/icons/lock.png"),
+  map: require("@/assets/icons/map.png"),
+  marker: require("@/assets/icons/marker.png"),
+  out: require("@/assets/icons/out.png"),
+  person: require("@/assets/icons/person.png"),
+  pin: require("@/assets/icons/pin.png"),
+  point: require("@/assets/icons/point.png"),
+  profile: require("@/assets/icons/profile.png"),
+  search: require("@/assets/icons/search.png"),
+  selectedMarker: require("@/assets/icons/selected-marker.png"),
+  star: require("@/assets/icons/star.png"),
+  target: require("@/assets/icons/target.png"),
+  to: require("@/assets/icons/to.png"),
+};
+
+export const images = {
+  onboarding1: require("@/assets/images/onboarding1.png"),
+  onboarding2: require("@/assets/images/onboarding2.png"),
+  onboarding3: require("@/assets/images/onboarding3.png"),
+  getStarted: require("@/assets/images/get-started.png"),
+  signUpCar: require("@/assets/images/signup-car.png"),
+  check: require("@/assets/images/check.png"),
+  noResult: require("@/assets/images/no-result.png"),
+  message: require("@/assets/images/message.png"),
+};
+
+export const onboarding = [
+  {
+    id: 1,
+    title: "The perfect ride is just a tap away!",
+    description:
+      "Your journey begins with Ryde. Find your ideal ride effortlessly.",
+    image: images.onboarding1,
+  },
+  {
+    id: 2,
+    title: "Best car in your hands with Ryde",
+    description:
+      "Discover the convenience of finding your perfect ride with Ryde",
+    image: images.onboarding2,
+  },
+  {
+    id: 3,
+    title: "Your ride, your way. Let's go!",
+    description:
+      "Enter your destination, sit back, and let us take care of the rest.",
+    image: images.onboarding3,
+  },
+];
+
+export const data = {
+  onboarding,
+};
